360c3fdfc1376e672768f54fb3ecd9595b21533c
Shishir <shishirbhandari1234@gmail.com>
2020-07-08 17:35:17 +0545

    Signup Layout Added

---
+++ app/src/main/java/com/example/meropasal/ui/auth/Signup.java
@@ -0,0 +1,16 @@
+package com.example.meropasal.ui.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.meropasal.R;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
