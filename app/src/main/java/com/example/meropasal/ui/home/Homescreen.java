e8fae051cecc2e788ce7c8817871bf8278ca7e33
amanda11233 <a_raaz@live.com>
2020-07-17 22:18:34 +0545

    Some Changes

--- app/src/main/java/com/example/meropasal/ui/home/Homescreen.java
+++ app/src/main/java/com/example/meropasal/ui/home/Homescreen.java
@@ -1,5 +1,6 @@
 package com.example.meropasal.ui.home;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,26 +9,36 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 import com.example.meropasal.R;
+import com.example.meropasal.adapters.ImageSliderAdapter;
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
 
 
 public class Homescreen extends Fragment {
-
-
+    SliderView sliderview;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_homescreen,container,false);
+        sliderview = root.findViewById(R.id.imageSlider);
 
 
 
-
-
-
-
-
+        //Instanciating the image-slider adapter in the buymeds fragment//
+        ImageSliderAdapter adapter = new ImageSliderAdapter(root.getContext());
+        sliderview.setSliderAdapter(adapter);
+        sliderview.setIndicatorAnimation(IndicatorAnimationType.DROP); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        sliderview.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        sliderview.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH);
+        sliderview.setIndicatorSelectedColor(Color.WHITE);
+        sliderview.setIndicatorUnselectedColor(Color.WHITE);
+        sliderview.setScrollTimeInSec(3); //set scroll delay in seconds :
+        sliderview.startAutoCycle();
 
 
 
